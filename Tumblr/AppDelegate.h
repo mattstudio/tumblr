b650cd804b83810bc308693fba0a9074c6ebb6b3
mattstudio <matt@usertesting.com>
2014-06-04 19:01:15 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Matt Mitchell on 6/4/14.
+//  Copyright (c) 2014 Matt Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
